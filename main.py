e560d1e41b4cc35ef3abdf833d023d29feb1ddd9
yanedgroup <yanedgroup@gmail.com>
2023-02-05 16:54:22 +0700

    add scoreboard class

--- main.py
+++ main.py
@@ -11,7 +11,9 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
+
 
 # screen setup
 screen = Screen()
@@ -23,6 +25,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 
 screen.listen()
@@ -33,14 +36,32 @@ screen.onkey(r_paddle.go_down, "l")
 
 
 game_is_on = True
+
 while game_is_on == True:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
     # detecting collision with wall
     if ball.ycor() > 290 or ball.ycor() < -290:
         # need to bounce
-        ball.bounce()
+        ball.bounce_y()
+        ball.move_speed *= 0.9
 
+    # detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+        ball.move_speed *= 0.9
+
+    # detect R paddle misses
+    if ball.xcor() > 360:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # detect L paddle misses
+    if ball.xcor() < -360:
+        ball.reset_position()
+        scoreboard.r_point()
+
+    scoreboard.update_score()
 screen.exitonclick()
